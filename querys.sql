41e538002e22799133c3858e7c802448ace588df
juan <ivolucion@outlook.es>
2021-03-31 00:22:26 -0500

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -15,5 +15,22 @@ where (f.fechaCompra>='2020-02-01' and f.fechaCompra<='2020-05-25')
 and c.fechaNacimiento>'1985-03-30'
 
 --Obtener el valor total vendido por cada producto en el año 2000
+Select c.nombre,sum(c.valorUnitario+f.cantidad) as valorTotalVendido
+from
+Producto c
+inner join FacturaDetalle f on f.productoId=c.productoId
+inner join factura fa on fa.facturaId=f.facturaId
+where (fa.fechaCompra>='2020-01-01' and fa.fechaCompra<='2020-12-31')
+GROUP BY c.nombre
+
 
 --Obtener la última fecha de compra de un cliente y según su frecuencia de compra estimar en qué fecha podría volver a comprar.
+Select c.nombres, max(f.fechaCompra) as ultimaCompra, 
+case when DATEDIFF(day, min(f.fechaCompra), max(f.fechaCompra))/count(1)=0 then null else
+DATEADD(day, (DATEDIFF(day, min(f.fechaCompra), max(f.fechaCompra))/count(1)), GETDATE())
+end as proximaCompra
+from
+Cliente c
+inner join factura f on f.clienteId=c.clienteId
+GROUP BY c.nombres
+
